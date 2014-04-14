72f9ab9dab87ced211f55e80967979ef0c25dcf4
Tõnis Tobre <tobre@bitweb.ee>
2014-04-14 13:38:22 +0300

    Initial commit

---
+++ config/module.config.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+    'subject' => '[Errors][your-app-id-here]',
+    'emails' => array(
+        'you@domain.com'
+    ),
+    'from_address' => 'you@domian.com',
+    'ignore404' => false,
+    'ignoreBot404' => false,
+    'botList' => array(
+        'AhrefsBot',
+        'bingbot',
+        'Ezooms',
+        'Googlebot',
+        'Mail.RU_Bot',
+        'YandexBot',
+    )
+);
---
+++ src/BitWeb/ErrorReportingModule/Module.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tobre
+ * Date: 14.04.14
+ * Time: 13:19
+ */
+
+namespace BitWeb\ErrorReportingModule;
+
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
